d0bd41e00c132d860ac42eb347a62a01d2dfd428
Marcelo J. Both <marcelo.both@gmail.com>
2014-04-21 10:07:57 -0300

    Added admin flag to user.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "user_name"
+    name "username"
     email "sample@example.com"
     password "hunter2"
     password_confirmation "hunter2"
+
+    factory :admin_user do
+      admin true
+    end
   end
 end
