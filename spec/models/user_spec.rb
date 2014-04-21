831fa5b8f24924527c58765c01a92ef6dd60a410
Marcelo J. Both <marcelo.both@gmail.com>
2014-04-21 18:28:01 -0300

    Add ability to edit and update users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,51 @@
 require 'spec_helper'
 
 describe User do
-  it "needs a password and confirmation to save" do
-    u = User.new(name: "Steve")
+  it "requires an email" do
+    u = User.new(name: "steve",
+                 password: "hunter2",
+                 password_confirmation: "hunter2")
 
     u.save
     expect(u).to_not be_valid
 
-    u.password = "password"
-    u.password_confirmation = ""
-    u.save
-    expect(u).to_not be_valid
-
-    u.password_confirmation = "password"
+    u.email = "steve@example.com"
     u.save
     expect(u).to be_valid
   end
 
-  it "needs password and confirmation to match" do
-    u = User.create(
-      name: "Steve",
-      password: "hunter2",
-      password_confirmation: "hunter")
+  describe "passwords" do
+    it "needs a password and confirmation to save" do
+      u = User.new(name: "steve", email: "steve@example.com")
 
-    expect(u).to_not be_valid
+      u.save
+      expect(u).to_not be_valid
+
+      u.password = "password"
+      u.password_confirmation = ""
+      u.save
+      expect(u).to_not be_valid
+
+      u.password_confirmation = "password"
+      u.save
+      expect(u).to be_valid
+    end
+
+    it "needs password and confirmation to match" do
+      u = User.create(
+            name: "steve",
+            email: "steve@example.com",
+            password: "hunter2",
+            password_confirmation: "hunter")
+
+      expect(u).to_not be_valid
+    end
   end
 
   describe "authentication" do
     let(:user) { User.create(
-      name: "Steve",
+      name: "steve",
+      email: "steve@example.com",
       password: "hunter2",
       password_confirmation: "hunter2") }
 
@@ -36,7 +53,7 @@ describe User do
       expect(user.authenticate("hunter2")).to be
     end
 
-    it "does not authenticates with a incorrect password" do
+    it "does not authenticate with an incorrect password" do
       expect(user.authenticate("hunter1")).to_not be
     end
   end
