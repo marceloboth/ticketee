5d36745df1841cb06e72dedc68f31296bf55b90c
Marcelo J. Both <marcelo.both@gmail.com>
2014-04-20 22:13:51 -0300

    Implement signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Ticketee::Application.routes.draw do
   root 'projects#index'
 
+  resources :users
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
+
   resources :projects do
     resources :tickets
   end
-
-  resources :users
 end
