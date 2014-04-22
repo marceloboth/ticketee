d7109938379a843264a0742fa04863109390f437
Marcelo J. Both <marcelo.both@gmail.com>
2014-04-21 21:38:28 -0300

    Don't show projects that a user doesn't have permission to see

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,10 @@
 class ProjectsController < ApplicationController
   before_action :authorize_admin!, except: [:show, :index]
-  before_action :require_signin!, only: [:show]
+  before_action :require_signin!, only: [:index, :show]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    @projects = Project.for(current_user)
   end
 
   def new
@@ -43,11 +43,7 @@ class ProjectsController < ApplicationController
 
   private
     def set_project
-      @project = if current_user.admin?
-        Project.find(params[:id])
-      else
-        Project.viewable_by(current_user).find(params[:id])
-      end
+      @project = Project.for(current_user).find(params[:id])
     rescue ActiveRecord::RecordNotFound
       flash[:alert] = "The project you were looking for could not be found."
       redirect_to projects_path
